eab79ca695bb90f6450a68a66c4c17b8fe862c5b
bibidy <bibidy17@gmail.com>
2017-05-06 19:36:24 +0300

    production

--- config/environments.rb
+++ config/environments.rb
@@ -15,14 +15,15 @@ end
 
 configure :production do
 	
-	db = URI.parse('postgresql://yurii:5101443ura@localhost/bus_schedule_pcod')
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgresql://localhost/bus_schedule_pcod')
 
 	ActiveRecord::Base.establish_connection(
-		:adapter  => db.scheme,
+		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme
 		:username => db.user,
 		:password => db.password,
 		:host 	  => db.host,
-		:database => db.path[1..-1]
+		:database => db.path[1..-1],
+		:encoding => 'utf8'
 	)
 end
 
